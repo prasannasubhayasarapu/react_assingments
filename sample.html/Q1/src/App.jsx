ef709ac4744923cea7e48d7cacf25ca4c2a8eccd
Your Name <you@example.com>
2025-12-12 11:04:28 +0530

    my first react

---
+++ sample.html/Q1/src/App.jsx
@@ -0,0 +1,9 @@
+import FirstComponent from "./FirstComponent";
+
+function App(){
+  return <div>
+ <FirstComponent></FirstComponent>
+
+  </div>
+}
+export default App;
---
+++ sample.html/Q1/src/FirstComponent.jsx
@@ -0,0 +1,4 @@
+function FirstComponent(){
+return <h1>This is my first React component</h1>
+}
+export default FirstComponent;
